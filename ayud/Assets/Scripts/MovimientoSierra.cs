5f5ebbcafdb246e7c82d01fca5fa7cbe14b9ed5e
Camilo <jcamilo@ciencias.unam.mx>
2025-03-04 20:22:09 -0600

    mejoras para crear objetos

--- ayud/Assets/Scripts/MovimientoSierra.cs
+++ ayud/Assets/Scripts/MovimientoSierra.cs
@@ -1,28 +1,72 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class MovimientoCierra : MonoBehaviour
 {
-    // Objeto prefab "chain"
-    [SerializeField] private GameObject chain;
+    // Objeto a crear
+    [SerializeField] private GameObject crear;
+    // Posicion inicial
+    [SerializeField] private Vector2 posicionInicial;
+    // Posicion final
+    [SerializeField] private Vector2 posicionFinal;
+    // Distancia de movimiento
+    private float distanciaMovimiento = 0;
+    // Cantidad objetos a crear
+    [SerializeField] private int cantidadObjetos;
+    // Lista de posiciones
+    private List<float> posiciones = new List<float>();
+    // Ultima posicion
+    private float ultimaPosicion = float.MaxValue;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        // Si la posicion inicial y final estan definidas
+        if (posicionInicial != null && posicionFinal != null)
+        {
+            // Calculamos la distancia de movimiento
+            distanciaMovimiento = posicionFinal.x - posicionInicial.x;
+            // Si la distancia de movimiento es diferente de 0 y la cantidad de objetos es mayor a 0
+            if (distanciaMovimiento != 0 && cantidadObjetos > 0)
+            {
+                // Dividimos la distancia de movimiento entre la cantidad de objetos
+                float distancia = distanciaMovimiento / cantidadObjetos;
+                // Agregamos las posiciones donde crearemos objetos
+                float pos_actual = posicionInicial.x;
+                for (int i = 0; i < cantidadObjetos; i++)
+                {
+                    posiciones.Add(pos_actual);
+                    pos_actual += distancia;
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Mover el objeto de izquierda a derecha (de x = -1 a x = 1) en un tiempo de 4 segundos
-        transform.position = new Vector3(Mathf.PingPong(Time.time / 4, 2) - 1, transform.position.y, transform.position.z);
-        // Al moverse, crear un objeto prefab "chain" si no hay uno en la posicion nueva, solo crear cada 1 segundo
-        if (Time.time % 1 < Time.deltaTime)
+        // Si la distancia de movimiento es diferente de 0
+        if (distanciaMovimiento != 0)
         {
-            if (!Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - 0.5f), 0.1f))
+            // Vamos a mover el objeto desde la posicion inicial a la final y luego de la final a la inicial
+            float movimiento = Mathf.PingPong(Time.time, distanciaMovimiento) / distanciaMovimiento;
+            transform.position = Vector2.Lerp(posicionInicial, posicionFinal, movimiento);
+            // Revisar si estamos en una posicion para crear un objeto
+            if (posiciones.Count > 0)
             {
-                Instantiate(chain, new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z), Quaternion.identity);
-                Debug.Log("Chain created");
+                float xActual = transform.position.x;
+                float xObjetivo = posiciones[0];
+                // Si el objeto esta en la posicion objetivo y no se ha creado un objeto en esa posicion
+                if (Mathf.Abs(xActual - xObjetivo) < 0.01f && xObjetivo != ultimaPosicion)
+                {
+                    // Creamos el objeto
+                    Vector3 posicionCreacion = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
+                    Instantiate(crear, posicionCreacion, Quaternion.identity);
+                    // Eliminamos la posicion de la lista
+                    posiciones.RemoveAt(0);
+                    // Actualizamos la ultima posicion
+                    ultimaPosicion = xObjetivo;
+                }
             }
         }
     }
