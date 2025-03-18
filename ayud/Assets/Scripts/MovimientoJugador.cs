3d46c401c4bba9071e69a45b1e04833c542dd0d5
Camilo <jcamilo@ciencias.unam.mx>
2025-03-18 12:54:34 -0600

    ayud 18/03/25

--- ayud/Assets/Scripts/MovimientoJugador.cs
+++ ayud/Assets/Scripts/MovimientoJugador.cs
@@ -4,12 +4,14 @@ public class MovimientoJugador : MonoBehaviour
 {
     // Vida
     public Vida vida;
+    // Collider
+    [SerializeField] private Collider2D col;
 
     // Update is called once per frame
     void Update()
     {
-        // Al tocar la tecla izquierda permedomos vida
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        // Al hacer click izquierdo y este sobre el objeto
+        if (Input.GetMouseButtonDown(0) && col.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition)))
         {
             vida.PerderVida(10);
             Debug.Log("Vida: " + vida.vidaActual);
