a7b60ba4d835b7691ee2c78b48e8a1bf5b7bb034
Camilo <jcamilo@ciencias.unam.mx>
2025-03-27 12:58:32 -0600

    ayud 27/03/25

--- ayud/Assets/Scripts/ControladorJugador.cs
+++ ayud/Assets/Scripts/ControladorJugador.cs
@@ -7,11 +7,15 @@ public class ControladorJugador : MonoBehaviour
 {
     // Movimiento en X
     private MovimientoX movimientoX { get; set; }
+    // Movimeinto en Y
+    private MovimientoY movimientoY { get; set; }
 
     void Start()
     {
         // Obtener el componente MovimientoX
         movimientoX = GetComponent<MovimientoX>();
+        // Obtener el componente MovimientoY
+        movimientoY = GetComponent<MovimientoY>();
     }
 
     void Update()
@@ -34,6 +38,14 @@ public class ControladorJugador : MonoBehaviour
         {
             movimientoX.Desacelerar();
             movimientoX.CambiarDireccion(0f);
+        }
+    }
+
+    public void Saltar(InputAction.CallbackContext context)
+    {
+        if (context.started && movimientoY.EnPiso())
+        {
+            movimientoY.Saltar();
         }
     }
 }
